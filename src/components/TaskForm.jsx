0051b56f7b53d241524de769291c6088958a9d29
Tran Ho <tranhonguyen.work@gmail.com>
2026-04-19 21:49:04 +0700

    update title validation

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 function TaskForm({ addTask }) {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("todo");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!title.trim()) return;
+    if (!title.trim()) {
+      setError("Title is required");
+      return;
+    }
 
+    setError("");
     addTask({
       id: Date.now().toString(),
       title,
@@ -20,30 +25,34 @@ function TaskForm({ addTask }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex gap-2 mb-4">
-      <input
-        className="flex-1 border rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-blue-400"
-        placeholder="Enter task..."
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-
-      <select
-        className="border rounded-lg px-2 py-2"
-        value={status}
-        onChange={(e) => setStatus(e.target.value)}
-      >
-        <option value="todo">Todo</option>
-        <option value="in-progress">In Progress</option>
-        <option value="done">Done</option>
-      </select>
-
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
-        type="submit"
-      >
-        Add
-      </button>
+    <form onSubmit={handleSubmit} className="  mb-4 ">
+      <div className="flex gap-2  mb-1">
+        <input
+          className="w-full outline-none border rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-400"
+          placeholder="Enter task..."
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            if (error) setError("");
+          }}
+        />
+        <select
+          className="border rounded-lg px-2 py-2"
+          value={status}
+          onChange={(e) => setStatus(e.target.value)}
+        >
+          <option value="todo">Todo</option>
+          <option value="in-progress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+      {error && <span className="text-red-500">{error}</span>}
     </form>
   );
 }
